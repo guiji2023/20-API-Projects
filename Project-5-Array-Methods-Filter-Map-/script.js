9642e67217c9ab4733e0cd423fe4460da0d645ad
guiji2007 <98149176+guiji2007@users.noreply.github.com>
2023-11-26 17:04:53 -0600

    project 5 complete

---
+++ Project-5-Array-Methods-Filter-Map-/script.js
@@ -0,0 +1,102 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+// Fetch random user and add money
+const getRandomUser =  async ()=> {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+// Double eveyones money
+const doubleMoney = ()=> {
+  data = data.map(user => {
+    return { name: user.name, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
+// Sort users by richest
+const sortByRichest = ()=> {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+// Filter only millionaires
+const showMillionaires =()=> {
+  data = data.filter(user => user.money > 1000000);
+
+  updateDOM();
+}
+
+// Calculate the total wealth
+const calculateWealth = ()=> {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Add new obj to data arr
+const addData = (obj)=> {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+const updateDOM = (providedData = data)=> {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+const formatMoney = number=> '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+(async()=>{
+
+    getRandomUser();
+    getRandomUser();
+    getRandomUser();
+
+    addUserBtn.addEventListener('click', getRandomUser);
+    doubleBtn.addEventListener('click', doubleMoney);
+    sortBtn.addEventListener('click', sortByRichest);
+    showMillionairesBtn.addEventListener('click', showMillionaires);
+    calculateWealthBtn.addEventListener('click', calculateWealth);
+
+
+})();
+
+
+
+// Event listeners
+
